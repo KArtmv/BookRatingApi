8c964446222c66fc85becc0661a89524d48fd622
Kyrylo Artemov <kirillartemov1993.com>
2024-09-18 12:42:13 +0200

    Create generic step -Replace creating steps by generic Step

--- src/main/java/ua/foxminded/bookrating/springBatch/step/StepConfiguration.java
+++ src/main/java/ua/foxminded/bookrating/springBatch/step/StepConfiguration.java
@@ -4,20 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.adapter.ItemWriterAdapter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.PlatformTransactionManager;
 import ua.foxminded.bookrating.dto.*;
 import ua.foxminded.bookrating.persistance.entity.*;
 import ua.foxminded.bookrating.springBatch.process.*;
-
-import java.sql.BatchUpdateException;
 
 @Configuration
 @RequiredArgsConstructor
@@ -30,92 +27,51 @@ public class StepConfiguration {
     public Step authorStep(FlatFileItemReader<AuthorCsvDto> reader,
                            AuthorItemProcess process,
                            ItemWriterAdapter<Author> writer) {
-        return new StepBuilder("authorStep", jobRepository)
-                .<AuthorCsvDto, Author>chunk(1000, transactionManager)
-                .reader(reader)
-                .processor(process)
-                .writer(writer)
-                .faultTolerant()
-                .skip(FlatFileParseException.class)
-                .skip(BatchUpdateException.class)
-                .skip(DataIntegrityViolationException.class)
-                .skipLimit(Integer.MAX_VALUE)
-                .taskExecutor(taskExecutor())
-                .listener(process)
-                .build();
+        return createStep(reader, process, writer, "authorStep");
     }
 
     @Bean
     public Step publisherStep(FlatFileItemReader<PublisherCsvDto> reader,
                               PublisherItemProcess process,
                               ItemWriterAdapter<Publisher> writer) {
-        return new StepBuilder("publisherStep", jobRepository)
-                .<PublisherCsvDto, Publisher>chunk(1000, transactionManager)
-                .reader(reader)
-                .processor(process)
-                .writer(writer)
-                .faultTolerant()
-                .skip(FlatFileParseException.class)
-                .skip(BatchUpdateException.class)
-                .skip(DataIntegrityViolationException.class)
-                .skipLimit(Integer.MAX_VALUE)
-                .taskExecutor(taskExecutor())
-                .listener(process)
-                .build();
+        return createStep(reader, process, writer, "publisherStep");
     }
 
     @Bean
     public Step userStep(FlatFileItemReader<UserCsvDto> reader,
                          UserItemProcessor process,
                          ItemWriterAdapter<User> writer) {
-        return new StepBuilder("userStep", jobRepository)
-                .<UserCsvDto, User>chunk(1000, transactionManager)
-                .reader(reader)
-                .processor(process)
-                .writer(writer)
-                .taskExecutor(taskExecutor())
-                .faultTolerant()
-                .skipLimit(20)
-                .skip(FlatFileParseException.class)
-                .build();
+        return createStep(reader, process, writer, "userStep");
     }
 
     @Bean
     public Step bookStep(FlatFileItemReader<BookCsvDto> reader,
                          BookItemProcess process,
                          ItemWriterAdapter<Book> writer) {
-        return new StepBuilder("bookStep", jobRepository)
-                .<BookCsvDto, Book>chunk(1000, transactionManager)
-                .reader(reader)
-                .processor(process)
-                .writer(writer)
-                .faultTolerant()
-                .skipLimit(100)
-                .skip(FlatFileParseException.class)
-                .skip(DataIntegrityViolationException.class)
-                .taskExecutor(taskExecutor())
-                .listener(process)
-                .build();
+        return createStep(reader, process, writer, "bookStep");
+
     }
 
     @Bean
     public Step ratingStep(FlatFileItemReader<RatingCsvDto> reader,
                            RatingItemProcess process,
                            ItemWriterAdapter<Rating> writer) {
-        return new StepBuilder("ratingStep", jobRepository)
-                .<RatingCsvDto, Rating>chunk(1000, transactionManager)
+        return createStep(reader, process, writer, "ratingStep");
+    }
+
+    private <T, S> Step createStep(FlatFileItemReader<T> reader,
+                                   ItemProcessor<T, S> process,
+                                   ItemWriterAdapter<S> writer,
+                                   String name) {
+        return new StepBuilder(name, jobRepository)
+                .<T, S>chunk(1000, transactionManager)
                 .reader(reader)
                 .processor(process)
                 .writer(writer)
                 .faultTolerant()
-                .skipLimit(100)
                 .skip(FlatFileParseException.class)
-                .taskExecutor(taskExecutor())
-                .listener(process)
+                .skipLimit(100)
+                .taskExecutor(new SimpleAsyncTaskExecutor())
                 .build();
-    }
-
-    public TaskExecutor taskExecutor() {
-        return new SimpleAsyncTaskExecutor("asyncTaskExecutor");
     }
 }
