d3ebc4e2e398710e3bef68971895ca46b76e836d
Kyrylo <kirillartemov1993@gmail.com>
2024-10-29 14:58:02 +0100

    Create BookServiceImpl -Implement BookService

--- src/main/java/ua/foxminded/bookrating/service/implementation/BookServiceImpl.java
+++ src/main/java/ua/foxminded/bookrating/service/implementation/BookServiceImpl.java
@@ -1,14 +1,96 @@
 package ua.foxminded.bookrating.service.implementation;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ua.foxminded.bookrating.dto.BookDto;
 import ua.foxminded.bookrating.persistance.entity.Book;
+import ua.foxminded.bookrating.persistance.repo.BookRepository;
+import ua.foxminded.bookrating.projection.BookRatingProjection;
+import ua.foxminded.bookrating.service.AuthorService;
 import ua.foxminded.bookrating.service.BookService;
+import ua.foxminded.bookrating.service.PublisherService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional(readOnly = true)
 public class BookServiceImpl extends AbstractServiceImpl<Book> implements BookService {
 
-    public BookServiceImpl(JpaRepository<Book, Long> repository) {
+    private final BookRepository bookRepository;
+    private final PublisherService publisherService;
+    private final AuthorService authorService;
+
+    public BookServiceImpl(BookRepository repository, PublisherService publisherService, AuthorService authorService) {
         super(repository);
+        this.bookRepository = repository;
+        this.publisherService = publisherService;
+        this.authorService = authorService;
+    }
+
+    @Override
+    public Page<BookRatingProjection> findAllPaginated(Integer desiredAverageRating, Pageable pageable) {
+        return bookRepository.findAllPaginated(desiredAverageRating, pageable);
+    }
+
+    @Transactional
+    @Override
+    public Book save(BookDto entity) {
+        if (existByIsbn(entity.getIsbn())) {
+            return bookRepository.save(toEntity(entity, new Book()));
+        }
+        throw new EntityExistsException("Book with isbn " + entity.getIsbn() + " already exists");
+    }
+
+    @Transactional
+    @Override
+    public Book update(Long id, BookDto entity) {
+        return bookRepository.save(toEntity(entity, findById(id)));
+    }
+
+    @Override
+    public Book getByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public boolean existByIsbn(String isbn) {
+        return bookRepository.existsByIsbn(isbn);
+    }
+
+    @Override
+    public Page<BookRatingProjection> getByTitleContaining(String title, Pageable pageable) {
+        return bookRepository.findByTitleContainingIgnoreCase(title, pageable);
+    }
+
+    @Override
+    public Page<BookRatingProjection> getBooksByAuthorAndPublisher(List<Long> authorsId,
+                                                                   List<Long> publisherId,
+                                                                   Integer desiredAverageRating,
+                                                                   String title,
+                                                                   Pageable pageable) {
+        return bookRepository.findByAuthorsOrPublisherIn(
+                authorsId.isEmpty() ? Collections.emptyList() : authorsId.stream().map(authorService::findById).toList(),
+                publisherId.isEmpty() ? Collections.emptyList() : publisherId.stream().map(publisherService::findById).toList(),
+                desiredAverageRating,
+                title.trim(),
+                pageable);
+    }
+
+    private Book toEntity(BookDto dto, Book book) {
+        book.setIsbn(dto.getIsbn());
+        book.setTitle(dto.getTitle());
+        book.setPublicationYear(dto.getPublicationYear());
+        book.setPublisher(publisherService.findById(dto.getPublisherId()));
+        book.setAuthors(book.getAuthors().stream().map(a -> authorService.findById(a.getId())).collect(Collectors.toSet()));
+        book.getImage().setImageUrlSmall(dto.getImageUrlS());
+        book.getImage().setImageUrlMedium(dto.getImageUrlM());
+        book.getImage().setImageUrlLarge(dto.getImageUrlL());
+        return book;
     }
 }
