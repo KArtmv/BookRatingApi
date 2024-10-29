dd5944cef5aa25607dc84d485ff5d08279386ca2
Kyrylo <kirillartemov1993@gmail.com>
2024-10-29 11:32:45 +0100

    Create AuthorService extends by BaseService

--- src/main/java/ua/foxminded/bookrating/service/AuthorService.java
+++ src/main/java/ua/foxminded/bookrating/service/AuthorService.java
@@ -1,8 +1,9 @@
 package ua.foxminded.bookrating.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import ua.foxminded.bookrating.persistance.entity.Author;
+import ua.foxminded.bookrating.projection.BookRatingProjection;
 
-import java.util.Set;
-
-public interface AuthorService extends AbstractService<Author, Long> {
+public interface AuthorService extends BaseService<Author> {
 }
