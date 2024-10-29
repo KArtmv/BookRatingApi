55bbf11547f6ad8715fb513054350c7fe4a76209
Kyrylo <kirillartemov1993@gmail.com>
2024-10-29 11:11:41 +0100

    Create AuthorRepository -Add getBooksByAuthor method

--- src/main/java/ua/foxminded/bookrating/persistance/repo/AuthorRepository.java
+++ src/main/java/ua/foxminded/bookrating/persistance/repo/AuthorRepository.java
@@ -1,12 +1,31 @@
 package ua.foxminded.bookrating.persistance.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.foxminded.bookrating.persistance.entity.Author;
+import ua.foxminded.bookrating.projection.BookProjection;
+import ua.foxminded.bookrating.projection.AuthorProjection;
+import ua.foxminded.bookrating.projection.BookRatingProjection;
 
 import java.util.Optional;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-    Optional<Author> findByName(String name);
+public interface AuthorRepository extends BaseRepository<Author, Long> {
+
+    @Query("""
+            SELECT b as book, AVG(r.bookRating) AS averageRating
+            FROM Book b
+            JOIN b.authors a
+            JOIN b.ratings r
+            WHERE a = :author
+            GROUP BY b
+            HAVING AVG(r.bookRating) > :desiredAverageRating
+            """)
+    Page<BookRatingProjection> getBooksByAuthor(@Param("author") Author author,
+                                                @Param("desiredAverageRating") Integer desiredAverageRating,
+                                                Pageable pageable);
 }
