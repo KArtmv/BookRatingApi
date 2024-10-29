f600844b18c575c41454c3b82f86e0906f8cee22
Kyrylo <kirillartemov1993@gmail.com>
2024-10-29 14:44:33 +0100

    Create BookService -Add methods: findAllPaganated, save, update, getByIsbn,  existByIsbn, getByTitleContaining, getByAuthorAndPublisher

--- src/main/java/ua/foxminded/bookrating/service/BookService.java
+++ src/main/java/ua/foxminded/bookrating/service/BookService.java
@@ -1,6 +1,26 @@
 package ua.foxminded.bookrating.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import ua.foxminded.bookrating.dto.BookDto;
 import ua.foxminded.bookrating.persistance.entity.Book;
+import ua.foxminded.bookrating.projection.BookRatingProjection;
 
-public interface BookService extends AbstractService<Book, Long> {
+import java.util.List;
+
+public interface BookService extends AbstractService<Book> {
+    Page<BookRatingProjection> findAllPaginated(Integer desiredAverageRating, Pageable pageable);
+
+    Book save(BookDto entity);
+
+    Book update(Long id, BookDto entity);
+
+    Book getByIsbn(String isbn);
+
+    boolean existByIsbn(String isbn);
+
+    Page<BookRatingProjection> getByTitleContaining(String title, Pageable pageable);
+
+    Page<BookRatingProjection> getBooksByAuthorAndPublisher(List<Long> authorsId, List<Long> publishersId, Integer desiredAverageRating, String title, Pageable pageable);
 }
