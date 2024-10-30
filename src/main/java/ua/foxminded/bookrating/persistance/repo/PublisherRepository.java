d3acfe2d433a386f60b0f65519b15c1e03aa084e
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 08:57:14 +0100

    Create Publisher repository -Add getBooksByPublisher method

--- src/main/java/ua/foxminded/bookrating/persistance/repo/PublisherRepository.java
+++ src/main/java/ua/foxminded/bookrating/persistance/repo/PublisherRepository.java
@@ -1,12 +1,26 @@
 package ua.foxminded.bookrating.persistance.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.foxminded.bookrating.persistance.entity.Publisher;
+import ua.foxminded.bookrating.projection.BookRatingProjection;
 
 import java.util.Optional;
 
 @Repository
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-    Optional<Publisher> findByName(String name);
+public interface PublisherRepository extends BaseRepository<Publisher, Long> {
+
+    @Query("select b AS book, AVG(r.bookRating) AS averageRating " +
+            "from Book b " +
+            "JOIN Rating r ON b.id = r.book.id " +
+            "where b.publisher = :publisher " +
+            "group by b.id " +
+            "having AVG(r.bookRating) > :desiredAverageRating")
+    Page<BookRatingProjection> getBooksByPublisher(@Param("publisher") Publisher publisher,
+                                                   @Param("desiredAverageRating") Integer desiredAverageRating,
+                                                   Pageable pageable);
 }
