b34a226575e74f86d50f9c94aef3b0315e1a3204
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 09:25:02 +0100

    Create PublisherModelAssembler -Implement RepresentationModelAssembler -Override toModel method -Add methods of creating links to:  publisher self, publisher books

---
+++ src/main/java/ua/foxminded/bookrating/assembler/PublisherModelAssembler.java
@@ -0,0 +1,34 @@
+package ua.foxminded.bookrating.assembler;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import ua.foxminded.bookrating.controller.PublisherController;
+import ua.foxminded.bookrating.model.PublisherModel;
+import ua.foxminded.bookrating.persistance.entity.Publisher;
+import ua.foxminded.bookrating.service.PublisherService;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class PublisherModelAssembler implements RepresentationModelAssembler<Publisher, PublisherModel> {
+
+    @Override
+    public PublisherModel toModel(Publisher entity) {
+        PublisherModel publisherModel = new PublisherModel();
+        publisherModel.setId(entity.getId());
+        publisherModel.setName(entity.getName());
+        publisherModel.add(getSelfLink(entity));
+        publisherModel.add(getPublisherBooksLink(entity));
+        return publisherModel;
+    }
+
+    private Link getPublisherBooksLink(Publisher entity) {
+        return linkTo(methodOn(PublisherController.class).getPublisherBooks(entity.getId(), Pageable.unpaged(), 0)).withRel("publisherBooks");
+    }
+
+    private Link getSelfLink(Publisher entity) {
+        return linkTo(methodOn(PublisherController.class).getPublisher(entity.getId())).withSelfRel();
+    }
+}
