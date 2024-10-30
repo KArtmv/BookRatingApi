832e2d0ac4c3ddf043bec32191e8f81784566306
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 08:59:22 +0100

    Create PublisherService interface -Extend by BaseService

--- src/main/java/ua/foxminded/bookrating/service/PublisherService.java
+++ src/main/java/ua/foxminded/bookrating/service/PublisherService.java
@@ -2,7 +2,5 @@ package ua.foxminded.bookrating.service;
 
 import ua.foxminded.bookrating.persistance.entity.Publisher;
 
-import java.util.Set;
-
-public interface PublisherService extends AbstractService<Publisher, Long> {
+public interface PublisherService extends BaseService<Publisher> {
 }
