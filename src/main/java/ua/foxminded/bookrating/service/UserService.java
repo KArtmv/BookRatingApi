4f626f8507fd2ff29d9fdea0ce7259c415b3ad8a
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 11:29:49 +0100

    Create UserService interface -Add findRatedBooksByUser method

--- src/main/java/ua/foxminded/bookrating/service/UserService.java
+++ src/main/java/ua/foxminded/bookrating/service/UserService.java
@@ -1,6 +1,10 @@
 package ua.foxminded.bookrating.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ua.foxminded.bookrating.persistance.entity.Rating;
 import ua.foxminded.bookrating.persistance.entity.User;
 
-public interface UserService extends AbstractService<User, Long> {
+public interface UserService extends AbstractService<User> {
+    Page<Rating> findRatedBooksByUser(Long userId, Pageable pageable);
 }
