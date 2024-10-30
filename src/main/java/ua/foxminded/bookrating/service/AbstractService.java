e1fc9ef068fd38a54d2ab9f08f0815dd23ede476
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 08:42:01 +0100

    Create AbstractService -Add methods: findById, findAll, delete, update, save

--- src/main/java/ua/foxminded/bookrating/service/AbstractService.java
+++ src/main/java/ua/foxminded/bookrating/service/AbstractService.java
@@ -2,15 +2,16 @@ package ua.foxminded.bookrating.service;
 
 import ua.foxminded.bookrating.persistance.entity.BaseEntity;
 
-import java.io.Serializable;
 import java.util.List;
 
-public interface AbstractService<T extends BaseEntity, ID extends Serializable> {
-    T findById(ID id);
+public interface AbstractService<T extends BaseEntity> {
+    T findById(Long id);
 
     List<T> findAll();
 
-    void save(T entity);
+    void delete(Long id);
 
-    void delete(T entity);
+    T update(Long id, T entity);
+
+    T save(T entity);
 }
