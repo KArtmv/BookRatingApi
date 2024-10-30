782d969b88168c7d97ccabee612612398fbccac7
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 11:31:57 +0100

    Creaet UserServiceImpl -Extend by AbstractServiceImpl -Implement UserService interface

--- src/main/java/ua/foxminded/bookrating/service/implementation/UserServiceImpl.java
+++ src/main/java/ua/foxminded/bookrating/service/implementation/UserServiceImpl.java
@@ -1,14 +1,26 @@
 package ua.foxminded.bookrating.service.implementation;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import ua.foxminded.bookrating.persistance.entity.Rating;
 import ua.foxminded.bookrating.persistance.entity.User;
+import ua.foxminded.bookrating.persistance.repo.UserRepository;
 import ua.foxminded.bookrating.service.UserService;
 
 @Service
 public class UserServiceImpl extends AbstractServiceImpl<User> implements UserService {
 
-    public UserServiceImpl(JpaRepository<User, Long> repository) {
-        super(repository);
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        super(userRepository);
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Page<Rating> findRatedBooksByUser(Long userId, Pageable pageable) {
+        return userRepository.findByRatingsUser(findById(userId), pageable);
     }
 }
