949f4836a29d7730f062d88b69977ba68f39688a
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 11:34:05 +0100

    Create RatingOfBookModelAssembler -Implement toModel method of RepresantationModelAssembler

---
+++ src/main/java/ua/foxminded/bookrating/assembler/RatingOfBookModelAssembler.java
@@ -0,0 +1,35 @@
+package ua.foxminded.bookrating.assembler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import ua.foxminded.bookrating.controller.BookController;
+import ua.foxminded.bookrating.controller.RatingController;
+import ua.foxminded.bookrating.model.RatingModel;
+import ua.foxminded.bookrating.persistance.entity.Book;
+import ua.foxminded.bookrating.persistance.entity.Rating;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+@RequiredArgsConstructor
+public class RatingOfBookModelAssembler implements RepresentationModelAssembler<Rating, RatingModel> {
+
+    private final SimpleBookModelAssembler bookModelAssembler;
+
+    @Override
+    public RatingModel toModel(Rating entity) {
+        RatingModel model = RatingModel.builder()
+                .id(entity.getId())
+                .book(bookModelAssembler.toModel(entity.getBook()))
+                .rating(entity.getBookRating())
+                .build();
+        model.add(getSelfLink(entity));
+        return model;
+    }
+
+    private Link getSelfLink(Rating rating) {
+        return linkTo(methodOn(RatingController.class).get(rating.getId())).withSelfRel();
+    }
+}
