c117a8f783c4826706c16756ac2bb7997f2c3c38
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 09:09:04 +0100

    Create PublisherServiceImpl -Extend by BaseService -Implement PublisherService -Override methods: getBookById, save, update

--- src/main/java/ua/foxminded/bookrating/service/implementation/PublisherServiceImpl.java
+++ src/main/java/ua/foxminded/bookrating/service/implementation/PublisherServiceImpl.java
@@ -1,16 +1,46 @@
 package ua.foxminded.bookrating.service.implementation;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.EntityExistsException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.foxminded.bookrating.persistance.entity.Publisher;
+import ua.foxminded.bookrating.persistance.repo.PublisherRepository;
+import ua.foxminded.bookrating.projection.BookRatingProjection;
 import ua.foxminded.bookrating.service.PublisherService;
 
-import java.util.Set;
-
 @Service
-public class PublisherServiceImpl extends AbstractServiceImpl<Publisher> implements PublisherService {
+@Transactional(readOnly = true)
+public class PublisherServiceImpl extends BaseServiceImpl<Publisher> implements PublisherService {
 
-    public PublisherServiceImpl(JpaRepository<Publisher, Long> repository) {
+    private final PublisherRepository publisherRepository;
+
+    public PublisherServiceImpl(PublisherRepository repository) {
         super(repository);
+        this.publisherRepository = repository;
+    }
+
+    @Override
+    public Page<BookRatingProjection> getAllBooksById(Long id, Integer desiredAverageRating, Pageable pageable) {
+        return publisherRepository.getBooksByPublisher(findById(id), desiredAverageRating, pageable);
+    }
+
+
+    @Override
+    @Transactional
+    public Publisher save(Publisher publisher) {
+        if (!super.existsByName(publisher.getName())) {
+            return publisherRepository.save(publisher);
+        }
+        throw new EntityExistsException("Publisher with name " + publisher.getName() + " already exists");
+    }
+
+    @Override
+    @Transactional
+    public Publisher update(Long id, Publisher newPublisher) {
+        Publisher publisher = findById(id);
+        publisher.setName(newPublisher.getName());
+        return publisherRepository.save(publisher);
     }
 }
