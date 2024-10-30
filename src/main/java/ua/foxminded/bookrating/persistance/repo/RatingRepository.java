f774023e06ecc20493bfd8147c53e2a22d7a9696
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 09:41:18 +0100

    Create RatingRepository -Add method: getRatingByBook

--- src/main/java/ua/foxminded/bookrating/persistance/repo/RatingRepository.java
+++ src/main/java/ua/foxminded/bookrating/persistance/repo/RatingRepository.java
@@ -1,9 +1,13 @@
 package ua.foxminded.bookrating.persistance.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ua.foxminded.bookrating.persistance.entity.Book;
 import ua.foxminded.bookrating.persistance.entity.Rating;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+    Page<Rating> getRatingsByBook(Book book, Pageable pageable);
 }
