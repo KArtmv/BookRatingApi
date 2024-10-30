c159b8726f28241a8de149b1b6b530b36a4bc87f
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 10:06:11 +0100

    Create RatingService -Extend by AbstractService -Add methods: getRatingsByBookId, save, update -Create RatingServiceImpl -Extend by AbstractServiceImpl -Implement RatingService interface

--- src/main/java/ua/foxminded/bookrating/service/RatingService.java
+++ src/main/java/ua/foxminded/bookrating/service/RatingService.java
@@ -1,6 +1,14 @@
 package ua.foxminded.bookrating.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ua.foxminded.bookrating.dto.RatingDto;
 import ua.foxminded.bookrating.persistance.entity.Rating;
 
-public interface RatingService extends AbstractService<Rating, Long> {
+public interface RatingService extends AbstractService<Rating> {
+    Page<Rating> getRatingsByBookId(Long bookId, Pageable pageable);
+
+    Rating save(RatingDto ratingDto);
+
+    Rating update(Long id, Integer newRating);
 }
