bdbe43266f88fef655d85f7001cd4d50aee52b3e
Kyrylo <kirillartemov1993@gmail.com>
2024-10-30 11:36:49 +0100

    Create UserModelAssembler
    -Implement toModel method of RepresantationModelAssembler
    -Add methods of creating links to:
     user self, rated books by user

---
+++ src/main/java/ua/foxminded/bookrating/assembler/UserModelAssembler.java
@@ -0,0 +1,34 @@
+package ua.foxminded.bookrating.assembler;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import ua.foxminded.bookrating.controller.UserController;
+import ua.foxminded.bookrating.model.UserModel;
+import ua.foxminded.bookrating.persistance.entity.User;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class UserModelAssembler implements RepresentationModelAssembler<User, UserModel> {
+
+    @Override
+    public UserModel toModel(User entity) {
+        UserModel userModel = new UserModel();
+        userModel.setId(entity.getId());
+        userModel.setLocation(entity.getLocation());
+        userModel.setAge(entity.getAge());
+        userModel.add(getRatedBooksLink(entity));
+        userModel.add(getSelfLink(entity));
+        return userModel;
+    }
+
+    private Link getRatedBooksLink(User user) {
+        return linkTo(methodOn(UserController.class).getRatedBooksByUser(user.getId(), Pageable.unpaged())).withRel("ratedBooks");
+    }
+
+    private Link getSelfLink(User user) {
+        return linkTo(methodOn(UserController.class).getUser(user.getId())).withSelfRel();
+    }
+}
