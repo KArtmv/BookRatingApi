7f48f98ed30f0e9381a693833d8703ecc8df9daf
Kyrylo <kirillartemov1993@gmail.com>
2024-11-05 18:40:58 +0100

    Remove unnecessary class field

--- src/main/java/ua/foxminded/bookrating/exception/ItemNotFoundException.java
+++ src/main/java/ua/foxminded/bookrating/exception/ItemNotFoundException.java
@@ -2,8 +2,6 @@ package ua.foxminded.bookrating.exception;
 
 public class ItemNotFoundException extends RuntimeException {
 
-    private Long itemId;
-
     public ItemNotFoundException(Long itemId) {
         super("Item not found with id " + itemId);
     }
