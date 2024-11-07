a37260c4af2d36e1b46480e3e6e1e2be24888e84
Kyrylo <kirillartemov1993@gmail.com>
2024-11-07 14:46:40 +0100

    Add constructor with message parametr

--- src/main/java/ua/foxminded/bookrating/exception/ItemNotFoundException.java
+++ src/main/java/ua/foxminded/bookrating/exception/ItemNotFoundException.java
@@ -3,6 +3,10 @@ package ua.foxminded.bookrating.exception;
 public class ItemNotFoundException extends RuntimeException {
 
     public ItemNotFoundException(Long itemId) {
-        super("Item not found with id " + itemId);
+        super("Item not found with id: " + itemId);
+    }
+
+    public ItemNotFoundException(String message) {
+        super(message);
     }
 }
