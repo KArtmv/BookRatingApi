c4c1e25663749337bbd8b6d3b49349ac8901a776
Kyrylo <kirillartemov1993@gmail.com>
2024-11-07 16:29:04 +0100

    Create class contains test data of author

---
+++ src/test/java/ua/foxminded/bookrating/util/author/AuthorsData.java
@@ -0,0 +1,13 @@
+package ua.foxminded.bookrating.util.author;
+
+import lombok.Getter;
+import ua.foxminded.bookrating.persistance.entity.Author;
+
+@Getter
+public class AuthorsData {
+
+    private final Long id = 4L;
+    private final String name = "Scott Turow";
+    private final String authorTestName = "Test Name";
+    private final Author author = new Author(id, name);
+}
