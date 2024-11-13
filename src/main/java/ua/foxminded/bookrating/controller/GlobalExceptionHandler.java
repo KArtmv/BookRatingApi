16e43f7a41c6ed3831add63d81ba76a71494a270
Kyrylo <kirillartemov1993@gmail.com>
2024-11-13 19:39:55 +0100

    Replace ItemNotFoundException by EntityNotFoundException

--- src/main/java/ua/foxminded/bookrating/controller/GlobalExceptionHandler.java
+++ src/main/java/ua/foxminded/bookrating/controller/GlobalExceptionHandler.java
@@ -1,20 +1,18 @@
 package ua.foxminded.bookrating.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ua.foxminded.bookrating.exception.ItemNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ItemNotFoundException.class)
+    @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ProblemDetail handleEntityNotFoundException(ItemNotFoundException e) {
+    public ProblemDetail handleEntityNotFoundException(EntityNotFoundException e) {
         ProblemDetail detail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
         detail.setDetail(e.getMessage());
         return detail;
