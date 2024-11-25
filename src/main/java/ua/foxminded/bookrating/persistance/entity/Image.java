ce1af8fb926dd1405001fa216ec6c8b59cba4eaa
Kyrylo <kirillartemov1993@gmail.com>
2024-11-25 14:27:04 +0100

    Add equals and hashcode

--- src/main/java/ua/foxminded/bookrating/persistance/entity/Image.java
+++ src/main/java/ua/foxminded/bookrating/persistance/entity/Image.java
@@ -6,6 +6,9 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -32,5 +35,21 @@ public class Image extends BaseEntity {
         this.imageUrlSmall = imageUrlSmall;
         this.imageUrlMedium = imageUrlMedium;
         this.imageUrlLarge = imageUrlLarge;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Image image = (Image) o;
+        return getId() != null && Objects.equals(getId(), image.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
