1139744d6430ddc6e0e52d3d7f2ec2683e5afd42
Kyrylo <kirillartemov1993@gmail.com>
2024-11-25 14:24:49 +0100

    Exctract images variables to own fields

--- src/test/java/ua/foxminded/bookrating/util/image/ImageData.java
+++ src/test/java/ua/foxminded/bookrating/util/image/ImageData.java
@@ -6,11 +6,9 @@ import ua.foxminded.bookrating.persistance.entity.Image;
 @Getter
 public class ImageData {
     private final Long id = 110464L;
-    private final Image image = new Image(id, "http://images.amazon.com/images/P/0736688390.01.THUMBZZZ.jpg",
-            "http://images.amazon.com/images/P/0736688390.01.MZZZZZZZ.jpg",
-            "http://images.amazon.com/images/P/0736688390.01.LZZZZZZZ.jpg");
-
-    private final Image newImage = new Image("http://images.amazon.com/images/P/0736688390.01.THUMBZZZ.jpg",
-            "http://images.amazon.com/images/P/0736688390.01.MZZZZZZZ.jpg",
-            "http://images.amazon.com/images/P/0736688390.01.LZZZZZZZ.jpg");
+    private final String imageUrlS = "http://images.amazon.com/images/P/0736688390.01.THUMBZZZ.jpg";
+    private final String imageUrlM = "http://images.amazon.com/images/P/0736688390.01.MZZZZZZZ.jpg";
+    private final String imageUrlL = "http://images.amazon.com/images/P/0736688390.01.LZZZZZZZ.jpg";
+    private final Image image = new Image(id, imageUrlS, imageUrlM, imageUrlL);
+    private final Image newImage = new Image(imageUrlS, imageUrlM, imageUrlL);
 }
