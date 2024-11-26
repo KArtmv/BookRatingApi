224458a2084f3403bb7b36b173f0a469dc834706
Kyrylo <kirillartemov1993@gmail.com>
2024-11-26 21:28:09 +0100

    Add 'updatedRating' instanse -Add RatingDto instanse

--- src/test/java/ua/foxminded/bookrating/util/rating/RatingData.java
+++ src/test/java/ua/foxminded/bookrating/util/rating/RatingData.java
@@ -1,6 +1,7 @@
 package ua.foxminded.bookrating.util.rating;
 
 import lombok.Getter;
+import ua.foxminded.bookrating.dto.RatingDto;
 import ua.foxminded.bookrating.persistance.entity.Rating;
 import ua.foxminded.bookrating.util.book.BookData;
 import ua.foxminded.bookrating.util.user.UserData;
@@ -13,6 +14,8 @@ public class RatingData {
 
     private final Long id = 220481L;
     private final Integer userRating = 7;
+    private final Integer updatedRating = 5;
     private final Rating newRating = new Rating(BOOK_DATA.getBook(), USER_DATA.getUser(), userRating);
     private final Rating rating = new Rating(id, BOOK_DATA.getBook(), USER_DATA.getUser(), userRating);
+    private final RatingDto ratingDto = new RatingDto(BOOK_DATA.getId(), USER_DATA.getId(), userRating);
 }
