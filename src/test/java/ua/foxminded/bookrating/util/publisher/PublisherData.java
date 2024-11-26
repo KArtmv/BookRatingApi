cc409a26b042a8a8ac36454c391785fd850a13eb
Kyrylo <kirillartemov1993@gmail.com>
2024-11-26 14:40:47 +0100

    Rename 'publisherTestName' to 'updatedName' -Add publisher instance with 'updatedName'

--- src/test/java/ua/foxminded/bookrating/util/publisher/PublisherData.java
+++ src/test/java/ua/foxminded/bookrating/util/publisher/PublisherData.java
@@ -7,8 +7,9 @@ import ua.foxminded.bookrating.persistance.entity.Publisher;
 public class PublisherData {
     private final Long id = 1577L;
     private final String name = "Books on Tape";
-    private final String publisherTestName = "test name";
+    private final String updatedName = "test name";
     private final Publisher publisher = new Publisher(id, name);
     private final Publisher newPublisher = new Publisher(name);
+    private final Publisher updatedPublisher = new Publisher(updatedName);
 
 }
