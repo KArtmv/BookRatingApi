57b6a2f9e3bdd25260ceeb00a330e592058e196b
Kyrylo <kirillartemov1993@gmail.com>
2024-12-01 08:46:55 +0100

    Update parameters of bookStep method -Replace ItemWriterAdapter by SynchronizedItemStreamWriter -Update parameter of createStep method -Replace ItemWriterAdapter by ItemWriter

--- src/main/java/ua/foxminded/bookrating/springBatch/step/StepConfiguration.java
+++ src/main/java/ua/foxminded/bookrating/springBatch/step/StepConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.adapter.ItemWriterAdapter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileParseException;
+import org.springframework.batch.item.support.SynchronizedItemStreamWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
@@ -47,7 +49,7 @@ public class StepConfiguration {
     @Bean
     public Step bookStep(FlatFileItemReader<BookCsvDto> reader,
                          BookItemProcess process,
-                         ItemWriterAdapter<Book> writer) {
+                         SynchronizedItemStreamWriter<Book> writer) {
         return createStep(reader, process, writer, "bookStep");
 
     }
@@ -61,7 +63,7 @@ public class StepConfiguration {
 
     private <T, S> Step createStep(FlatFileItemReader<T> reader,
                                    ItemProcessor<T, S> process,
-                                   ItemWriterAdapter<S> writer,
+                                   ItemWriter<S> writer,
                                    String name) {
         return new StepBuilder(name, jobRepository)
                 .<T, S>chunk(1000, transactionManager)
