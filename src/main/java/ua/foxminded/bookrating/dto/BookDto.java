3b4bc8deccb77ae29be1782be46cd4216a98b153
Kyrylo <kirillartemov1993@gmail.com>
2024-12-02 19:49:58 +0100

    Add 'NotNull' annotation on 'publicationYear' and 'publisherId' fields -Add 'NotNull' annotation on authorId 'field' -Improve error message

--- src/main/java/ua/foxminded/bookrating/dto/BookDto.java
+++ src/main/java/ua/foxminded/bookrating/dto/BookDto.java
@@ -2,6 +2,8 @@ package ua.foxminded.bookrating.dto;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,18 +17,27 @@ import java.util.List;
 
 @Getter
 @Setter
-@Isbn(bookId = "id", isbn = "isbn", message = "ISBN already exists for another book.")
+@Isbn(bookId = "id", isbn = "isbn", message = "The provided ISBN already exists for another book.")
 public class BookDto {
     private Long id;
-    @NotBlank(message = "The ISBN of book is required")
-    @ISBN(type = ISBN.Type.ANY)
+
+    @NotBlank(message = "The ISBN of the book is required and cannot be blank.")
+    @ISBN(type = ISBN.Type.ANY, message = "The provided ISBN is not valid.")
     private String isbn;
-    @NotBlank(message = "The title of the book is required and cannot be empty")
+
+    @NotBlank(message = "The title of the book is required and cannot be empty.")
     private String title;
-    @PastOrPresent
+
+    @NotNull(message = "The publication year is required and cannot be null.")
+    @PastOrPresent(message = "The publication year must be in the past or the present.")
     private Year publicationYear;
+
+    @NotEmpty(message = "At least one author is required for the book.")
     private List<Long> authorsId;
+
+    @NotNull(message = "The publisher is required and cannot be null.")
     private Long publisherId;
+
     @Valid
     private Image image;
 }
