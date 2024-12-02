ca9c2b80ebbce195d94f54f726de7fd0212ac130
Kyrylo <kirillartemov1993@gmail.com>
2024-12-02 12:27:56 +0100

    Refactor book dto -Add 'NotBlank' and 'ISBN' annotation on isbn field -Add 'NotBlank' annotation on 'title' field -Replace 'publicationYear' field type by 'Year.class' -Add 'PresentOrPast' annotation on 'publicationYear' field -Add 'Isbn' annotation on class

--- src/main/java/ua/foxminded/bookrating/dto/BookDto.java
+++ src/main/java/ua/foxminded/bookrating/dto/BookDto.java
@@ -1,18 +1,32 @@
 package ua.foxminded.bookrating.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.ISBN;
+import org.hibernate.validator.constraints.URL;
+import ua.foxminded.bookrating.annotation.Isbn;
 import ua.foxminded.bookrating.persistance.entity.Image;
 
+import java.time.Year;
 import java.util.List;
 
 @Getter
 @Setter
+@Isbn(bookId = "id", isbn = "isbn", message = "ISBN already exists for another book.")
 public class BookDto {
+    private Long id;
+    @NotBlank(message = "The ISBN of book is required")
+    @ISBN(type = ISBN.Type.ANY)
     private String isbn;
+    @NotBlank(message = "The title of the book is required and cannot be empty")
     private String title;
-    private String publicationYear;
+    @PastOrPresent
+    private Year publicationYear;
     private List<Long> authorsId;
     private Long publisherId;
+    @Valid
     private Image image;
 }
