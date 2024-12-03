c93415d715c0681908523fae7fbf179106de2662
Kyrylo <kirillartemov1993@gmail.com>
2024-12-03 16:24:30 +0100

    Add bean validation -Add 'Validated' annotation on class -Add 'ISBN' annotation to 'isbn' param of 'getBookByIsbn' -Add 'NotBlank' annotation to 'title' param of 'getBooksContainTitle' -Replace 'authorId' and 'publishersId' params with BookFilterRequest -Add 'Valid' annotation to BookFilterRequest of 'getFilteredBooks'

--- src/main/java/ua/foxminded/bookrating/controller/BookController.java
+++ src/main/java/ua/foxminded/bookrating/controller/BookController.java
@@ -1,17 +1,21 @@
 package ua.foxminded.bookrating.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.ISBN;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ua.foxminded.bookrating.assembler.BookModelAssembler;
 import ua.foxminded.bookrating.assembler.FullBookModelAssembler;
 import ua.foxminded.bookrating.assembler.RatingModelAssembler;
 import ua.foxminded.bookrating.dto.BookDto;
+import ua.foxminded.bookrating.dto.BookFilterRequest;
 import ua.foxminded.bookrating.model.BookModel;
 import ua.foxminded.bookrating.model.RatingModel;
 import ua.foxminded.bookrating.model.SimpleBookModel;
@@ -19,11 +23,10 @@ import ua.foxminded.bookrating.persistance.entity.Rating;
 import ua.foxminded.bookrating.projection.BookRatingProjection;
 import ua.foxminded.bookrating.service.BookService;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Validated
 public class BookController {
 
     private final BookService bookService;
@@ -51,12 +54,12 @@ public class BookController {
     }
 
     @GetMapping("/books/isbn")
-    public BookModel getBookByIsbn(@RequestParam("isbn") String isbn) {
+    public BookModel getBookByIsbn(@RequestParam("isbn") @ISBN(type = ISBN.Type.ANY, message = "The provided ISBN is not valid") String isbn) {
         return fullBookModelAssembler.toModel(bookService.getByIsbn(isbn));
     }
 
     @GetMapping("/books/title")
-    public PagedModel<SimpleBookModel> getBooksContainTitle(@RequestParam("title") String title, @PageableDefault Pageable pageable) {
+    public PagedModel<SimpleBookModel> getBooksContainTitle(@RequestParam("title") @NotBlank(message = "The title cannot be blank or empty") String title, @PageableDefault Pageable pageable) {
         return pagedResourcesAssembler.toModel(bookService.getByTitleContaining(title, pageable), bookModelAssembler);
     }
 
@@ -78,15 +81,19 @@ public class BookController {
     }
 
     @GetMapping("/books/filter-by")
-    public PagedModel<SimpleBookModel> getFilteredBooks(@RequestParam("authorsId") List<Long> authorsId,
-                                                        @RequestParam("publishersId") List<Long> publishersId,
+    public PagedModel<SimpleBookModel> getFilteredBooks(@Valid @ModelAttribute BookFilterRequest filterRequest,
                                                         @RequestParam(value = "averageRating", required = false, defaultValue = "0") Integer averageRating,
                                                         @RequestParam(required = false, defaultValue = "") String title,
                                                         @PageableDefault Pageable pageable) {
-        return pagedResourcesAssembler.toModel(bookService.getBooksByAuthorAndPublisher(authorsId,
-                publishersId,
-                averageRating,
-                title,
-                pageable), bookModelAssembler);
+        return pagedResourcesAssembler.toModel(
+                bookService.getBooksByAuthorAndPublisher(
+                        filterRequest.authorsId(),
+                        filterRequest.publishersId(),
+                        averageRating,
+                        title,
+                        pageable
+                ),
+                bookModelAssembler
+        );
     }
 }
