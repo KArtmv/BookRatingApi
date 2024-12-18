abe7e9b1e37bd815fe8e080c32e2863020ac462a
Kyrylo <kirillartemov1993@gmail.com>
2024-12-18 16:05:25 +0100

    Add 'SoftDelete' annotation on class

--- src/main/java/ua/foxminded/bookrating/persistance/entity/BaseEntity.java
+++ src/main/java/ua/foxminded/bookrating/persistance/entity/BaseEntity.java
@@ -1,9 +1,12 @@
 package ua.foxminded.bookrating.persistance.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.SoftDeleteType;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -12,6 +15,7 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @MappedSuperclass
+@SoftDelete
 public class BaseEntity implements Serializable {
 
     @Serial
