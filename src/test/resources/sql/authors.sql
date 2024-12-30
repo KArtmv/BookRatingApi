d3faa28589b86ab3977af5ed66fd65926aff06c4
Kyrylo <kirillartemov1993@gmail.com>
2024-12-30 12:16:55 +0100

    Add deleted entries

--- src/test/resources/sql/authors.sql
+++ src/test/resources/sql/authors.sql
@@ -9,3 +9,20 @@ VALUES (4, 'Scott Turow'),
        (2406, 'Mary Renault'),
        (292, 'Barbara Taylor Bradford'),
        (23182, 'Christopher Dickey');
+
+INSERT INTO author (id, name, deleted)
+VALUES (487, 'Judith Rossner', true),
+       (72976, 'P. C. W Davies', false),
+       (43383, 'Carole Hyatt', false),
+       (43324, 'Terrence J. McCann', false),
+       (43248, 'Hayao Miyazaki', false),
+       (43321, 'Elizabeth Bruening Lewis', false),
+       (43310, 'Andy Gaus', false),
+       (211, 'Jack Canfield', false),
+       (92601, 'Barbara Goodfellow', false),
+       (4290, 'Thomas Keneally', false),
+       (100889, 'Laszlo Kubinyi', false),
+       (948, 'Patricia Highsmith', false),
+       (273, 'Dave Barry', false),
+       (214, 'Terry Pratchett', false),
+       (1476, 'James Kahn', false);
--- src/test/resources/sql/publishers.sql
+++ src/test/resources/sql/publishers.sql
@@ -9,3 +9,19 @@ VALUES (6, 'Farrar Straus Giroux'),
        (112, 'Warner Vision'),
        (1577, 'Books on Tape'),
        (564, 'Farrar, Straus and Giroux');
+
+INSERT INTO publisher (id, name, deleted)
+VALUES (1182, 'Simon and Schuster', true),
+       (5560, 'Executive Books', false),
+       (8129, 'Viz', false),
+       (8136, 'Dromedary Press', false),
+       (8135, 'Phanes Press', false),
+       (23, 'Health Communications', false),
+       (1953, 'Bookthrift Co', false),
+       (107, 'Warner Books (Mm)', false),
+       (329, 'Simon & Schuster', false),
+       (57, 'Rocket', false),
+       (116, 'Diogenes Verlag', false),
+       (10, 'Ballantine Books', false),
+       (413, 'Random House Childrens Books', false),
+       (79, 'Del Rey Books', false);
