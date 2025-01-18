dd92253e174addc710d72a84d7097040a283edfa
Kyrylo <kirillartemov1993@gmail.com>
2025-01-18 14:26:47 +0100

    Rename 'BaseRepository' instanse to 'baseRepository'

--- src/main/java/ua/foxminded/bookrating/service/implementation/RestoreServiceImpl.java
+++ src/main/java/ua/foxminded/bookrating/service/implementation/RestoreServiceImpl.java
@@ -1,19 +1,18 @@
 package ua.foxminded.bookrating.service.implementation;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import ua.foxminded.bookrating.persistance.entity.BaseEntity;
 import ua.foxminded.bookrating.persistance.repo.BaseRepository;
 
 public class RestoreServiceImpl<T extends BaseEntity> extends CrudServiceImpl<T> {
 
-    private final BaseRepository<T, Long> repository;
+    private final BaseRepository<T, Long> baseRepository;
 
-    protected RestoreServiceImpl(BaseRepository<T, Long> repository) {
-        super(repository);
-        this.repository = repository;
+    protected RestoreServiceImpl(BaseRepository<T, Long> baseRepository) {
+        super(baseRepository);
+        this.baseRepository = baseRepository;
     }
 
     public void restoreById(Long id) {
-        repository.restore(id);
+        baseRepository.restore(id);
     }
 }
