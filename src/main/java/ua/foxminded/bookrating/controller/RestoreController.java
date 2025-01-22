83eeda3c3568c2da627bc3b193eef35003315446
Kyrylo <kirillartemov1993@gmail.com>
2025-01-22 13:43:38 +0100

    Create generic controller -Add 'restore' method -Extend by CrudController

---
+++ src/main/java/ua/foxminded/bookrating/controller/RestoreController.java
@@ -0,0 +1,26 @@
+package ua.foxminded.bookrating.controller;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import ua.foxminded.bookrating.persistance.entity.BaseEntity;
+import ua.foxminded.bookrating.service.RestoreService;
+
+public class RestoreController<T extends BaseEntity, D, M extends RepresentationModel<?>> extends CrudController<T, D, M> {
+
+    private final RestoreService<T, D> restoreService;
+
+    protected RestoreController(RestoreService<T, D> restoreService, RepresentationModelAssembler<T, M> modelAssembler) {
+        super(restoreService, modelAssembler);
+        this.restoreService = restoreService;
+    }
+
+    @PutMapping("/{id}/restore")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void restore(@PathVariable Long id) {
+        restoreService.restoreById(id);
+    }
+}
