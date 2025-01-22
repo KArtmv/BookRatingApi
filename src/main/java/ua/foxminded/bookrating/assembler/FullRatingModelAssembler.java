c1fd1ebeaea740922b410692237ae61400ea0a66
Kyrylo <kirillartemov1993@gmail.com>
2025-01-22 10:59:22 +0100

    Add rating of book to build -Add rating self link

--- src/main/java/ua/foxminded/bookrating/assembler/FullRatingModelAssembler.java
+++ src/main/java/ua/foxminded/bookrating/assembler/FullRatingModelAssembler.java
@@ -1,10 +1,15 @@
 package ua.foxminded.bookrating.assembler;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+import ua.foxminded.bookrating.controller.RatingController;
 import ua.foxminded.bookrating.model.RatingModel;
 import ua.foxminded.bookrating.persistance.entity.Rating;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +24,12 @@ public class FullRatingModelAssembler implements RepresentationModelAssembler<Ra
                 .id(entity.getId())
                 .book(bookModelAssembler.toModel(entity.getBook()))
                 .user(userModelAssembler.toModel(entity.getUser()))
-                .build();
+                .rating(entity.getBookRating())
+                .build()
+                .add(getSelfLink(entity));
+    }
+
+    private Link getSelfLink(Rating rating) {
+        return linkTo(methodOn(RatingController.class).get(rating.getId())).withSelfRel();
     }
 }
