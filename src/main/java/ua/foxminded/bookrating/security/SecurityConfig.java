053fb06a4fd1388f007e2d8d1522abe32d2a3d56
Kyrylo <kirillartemov1993@gmail.com>
2025-01-24 11:58:41 +0100

    Fix audience -Replace 'O' by '0'

--- src/main/java/ua/foxminded/bookrating/security/SecurityConfig.java
+++ src/main/java/ua/foxminded/bookrating/security/SecurityConfig.java
@@ -19,7 +19,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${authO.audience}")
+    @Value("${auth0.audience}")
     private String audience;
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
