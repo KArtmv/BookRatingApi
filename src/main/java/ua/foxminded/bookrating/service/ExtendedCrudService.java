b7f6b9000749ecd7732dbb2c1d1cd3ef64bf98d3
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-03 20:11:42 +0100

    Refactor 'getAllBooksById' -Remove 'desiredAverageRating' parameter from 'getAlBooksById' -Replace 'BookRatingProjection' by 'Book'

--- src/main/java/ua/foxminded/bookrating/service/ExtendedCrudService.java
+++ src/main/java/ua/foxminded/bookrating/service/ExtendedCrudService.java
@@ -3,10 +3,11 @@ package ua.foxminded.bookrating.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import ua.foxminded.bookrating.persistance.entity.BaseEntity;
+import ua.foxminded.bookrating.persistance.entity.Book;
 import ua.foxminded.bookrating.projection.BookRatingProjection;
 
 public interface ExtendedCrudService<T extends BaseEntity, D> extends PaginatedService<T, D> {
-    Page<BookRatingProjection> getAllBooksById(Long id, Integer desiredAverageRating, Pageable pageRequest);
+    Page<Book> getAllBooksById(Long id, Pageable pageRequest);
 
     Page<T> getByNameContaining(String name, Pageable pageable);
 
