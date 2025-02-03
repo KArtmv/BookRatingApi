cf4cfd535df3caedb45f1915a6fd99504bde0aee
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-03 20:07:52 +0100

    Extend by 'PaginatedServiceImpl' -Remove duplicate 'fidAll' method -Remove 'desiredAverageRating' parameter from  'getAlBooksById' -Replace 'BookRatingProjection' by 'Book'

--- src/main/java/ua/foxminded/bookrating/service/implementation/ExtendedCrudServiceImpl.java
+++ src/main/java/ua/foxminded/bookrating/service/implementation/ExtendedCrudServiceImpl.java
@@ -4,22 +4,19 @@ import jakarta.persistence.EntityExistsException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import ua.foxminded.bookrating.persistance.entity.Book;
 import ua.foxminded.bookrating.persistance.entity.NamedEntity;
 import ua.foxminded.bookrating.persistance.repo.ExtendedRepository;
 import ua.foxminded.bookrating.projection.BookRatingProjection;
 
 @Transactional(readOnly = true)
-public class ExtendedCrudServiceImpl<T extends NamedEntity> extends RestoreServiceImpl<T> {
+public class ExtendedCrudServiceImpl<T extends NamedEntity> extends PaginatedServiceImpl<T> {
 
     private final ExtendedRepository<T, Long> extendedRepository;
 
     public ExtendedCrudServiceImpl(ExtendedRepository<T, Long> extendedRepository) {
         super(extendedRepository);
         this.extendedRepository = extendedRepository;
-    }
-
-    public Page<T> findAll(Pageable pageable) {
-        return extendedRepository.findAllPaginated(pageable);
     }
 
     @Override
@@ -43,8 +40,8 @@ public class ExtendedCrudServiceImpl<T extends NamedEntity> extends RestoreServi
         return extendedRepository.findByNameContainingIgnoreCase(name, pageable);
     }
 
-    public Page<BookRatingProjection> getAllBooksById(Long id, Integer desiredAverageRating, Pageable pageRequest) {
-        return extendedRepository.getBooksByEntity(findById(id), desiredAverageRating, pageRequest);
+    public Page<Book> getAllBooksById(Long id, Pageable pageRequest) {
+        return extendedRepository.getBooksByEntity(findById(id), pageRequest);
     }
 
     @Transactional
