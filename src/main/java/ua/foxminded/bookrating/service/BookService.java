dd2ee743c2547ab3fed6ceb7d8c69fa259199fa8
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-04 23:21:26 +0100

    Remove 'getByTitleContaining' and 'getNooksByAuthorAndPublisher' methods -Add 'getBooksWithFilters' method

--- src/main/java/ua/foxminded/bookrating/service/BookService.java
+++ src/main/java/ua/foxminded/bookrating/service/BookService.java
@@ -3,6 +3,7 @@ package ua.foxminded.bookrating.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import ua.foxminded.bookrating.dto.BookDto;
+import ua.foxminded.bookrating.dto.BookFilterRequest;
 import ua.foxminded.bookrating.persistance.entity.Book;
 import ua.foxminded.bookrating.persistance.entity.Rating;
 import ua.foxminded.bookrating.projection.BookRatingProjection;
@@ -12,9 +13,7 @@ import java.util.List;
 public interface BookService extends PaginatedService<Book, BookDto> {
     Book getByIsbn(String isbn);
 
-    Page<BookRatingProjection> getByTitleContaining(String title, Pageable pageable);
-
-    Page<BookRatingProjection> getBooksByAuthorAndPublisher(List<Long> authorsId, List<Long> publishersId, Integer desiredAverageRating, String title, Pageable pageable);
+    Page<Book> getBooksWithFilters(String title, List<Long> authorIds, List<Long> publisherIds, Integer publicationTear, Integer averageRating, Pageable pageable);
 
     Page<Rating> getRatingsByBookId(Long id, Pageable pageable);
 }
