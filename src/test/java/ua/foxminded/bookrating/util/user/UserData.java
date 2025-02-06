a387b9e6e1e8554c960a568cec41008480d1670e
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-06 13:38:52 +0100

    Add 'updatedLocation' variable -Add 'updatedAge' variable -Add 'updatedUser' variable

--- src/test/java/ua/foxminded/bookrating/util/user/UserData.java
+++ src/test/java/ua/foxminded/bookrating/util/user/UserData.java
@@ -11,6 +11,9 @@ public class UserData {
     private final User user = new User(id, location, age);
     private final User newUser = new User("stockton, california, usa", 18);
     private final Long deletedUserId = 55187L;
+    private final String updatedLocation = "Mariupol, Donecka, Ukraine";
+    private final Integer updatedAge = 32;
+    private final User updatedUser = new User(updatedLocation, updatedAge);
 
     private final String userRatedBooksHref = "http://localhost/api/v1/users/58792/rated-books";
     private final String selfHref = "http://localhost/api/v1/users/58792";
