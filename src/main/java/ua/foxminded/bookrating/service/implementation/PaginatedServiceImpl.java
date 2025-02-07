e43adcc1e57ead00c18110a00a3e419ea71618ae
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-07 14:01:21 +0100

    Replace 'BaseReposytory' to 'PagingAndSortingRepository'

--- src/main/java/ua/foxminded/bookrating/service/implementation/PaginatedServiceImpl.java
+++ src/main/java/ua/foxminded/bookrating/service/implementation/PaginatedServiceImpl.java
@@ -2,19 +2,20 @@ package ua.foxminded.bookrating.service.implementation;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ua.foxminded.bookrating.persistance.entity.BaseEntity;
 import ua.foxminded.bookrating.persistance.repo.BaseRepository;
 
 public class PaginatedServiceImpl<T extends BaseEntity> extends RestoreServiceImpl<T> {
 
-    protected BaseRepository<T, Long> repository;
+    protected PagingAndSortingRepository<T, Long> pagingAndSortingRepository;
 
-    public PaginatedServiceImpl(BaseRepository<T, Long> repository) {
-        super(repository);
-        this.repository = repository;
+    public PaginatedServiceImpl(BaseRepository<T, Long> baseRepository) {
+        super(baseRepository);
+        this.pagingAndSortingRepository = baseRepository;
     }
 
     public Page<T> findAll(Pageable pageable) {
-        return repository.findAll(pageable);
+        return pagingAndSortingRepository.findAll(pageable);
     }
 }
