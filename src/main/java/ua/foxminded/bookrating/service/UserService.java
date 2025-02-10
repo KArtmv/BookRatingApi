844581fe8ed6e20be21925f31d7c7c8c12f6a4ac
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-10 10:18:26 +0100

    Replace type of 'D' parameter with 'UserDto' of 'RestoreService'

--- src/main/java/ua/foxminded/bookrating/service/UserService.java
+++ src/main/java/ua/foxminded/bookrating/service/UserService.java
@@ -2,9 +2,10 @@ package ua.foxminded.bookrating.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import ua.foxminded.bookrating.dto.UserDto;
 import ua.foxminded.bookrating.persistance.entity.Rating;
 import ua.foxminded.bookrating.persistance.entity.User;
 
-public interface UserService extends RestoreService<User, User> {
+public interface UserService extends RestoreService<User, UserDto> {
     Page<Rating> findRatedBooksByUser(Long userId, Pageable pageable);
 }
