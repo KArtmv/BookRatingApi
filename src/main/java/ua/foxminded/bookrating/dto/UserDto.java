d4091bdffd9f5e924b31ea850fc8418164cd0667
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-10 10:15:25 +0100

    Add validation on 'age' -Add 'PositiveOrZero' annotation

--- src/main/java/ua/foxminded/bookrating/dto/UserDto.java
+++ src/main/java/ua/foxminded/bookrating/dto/UserDto.java
@@ -1,6 +1,9 @@
 package ua.foxminded.bookrating.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 
-public record UserDto(@NotBlank(message = "Location can't be blank") String location, Integer age) {
+public record UserDto(@NotBlank(message = "Location can't be blank") String location,
+                      @PositiveOrZero(message = "Age can be only a positive value or zero") Integer age) {
 }
