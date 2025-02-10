8362e10d09f1d4c84e57ed44fd5dde20696a5eb4
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-10 16:34:30 +0100

    Add 'EnableMethodSecuriry' annotation

--- src/main/java/ua/foxminded/bookrating/security/SecurityConfig.java
+++ src/main/java/ua/foxminded/bookrating/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Value("${auth0.audience}")
