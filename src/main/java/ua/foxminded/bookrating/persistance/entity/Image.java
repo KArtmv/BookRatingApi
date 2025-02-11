9993ed75e35e01be6ba03c328d9007192d25c66b
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-11 19:18:02 +0100

    Replace 'NotBlank' with 'NotEmpty'

--- src/main/java/ua/foxminded/bookrating/persistance/entity/Image.java
+++ src/main/java/ua/foxminded/bookrating/persistance/entity/Image.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,21 +18,21 @@ public class Image {
 
     @Schema(description = "URL of the book's small-sized cover image",
             example = "http://images.amazon.com/images/P/0736688390.01.SMALL.jpg")
-    @NotBlank(message = "Small image URL is required")
+    @NotEmpty(message = "Small image URL is required")
     @URL(protocol = "http", message = "Small image URL must be a valid HTTP URL")
     @Column(name = "image_url_small")
     private String imageUrlSmall;
 
     @Schema(description = "URL of the book's medium-sized cover image",
             example = "http://images.amazon.com/images/P/0736688390.01.MEDIUM.jpg")
-    @NotBlank(message = "Medium image URL is required")
+    @NotEmpty(message = "Medium image URL is required")
     @URL(protocol = "http", message = "Medium image URL must be a valid HTTP URL")
     @Column(name = "image_url_medium")
     private String imageUrlMedium;
 
     @Schema(description = "URL of the book's large-sized cover image",
             example = "http://images.amazon.com/images/P/0736688390.01.LARGE.jpg")
-    @NotBlank(message = "Large image URL is required")
+    @NotEmpty(message = "Large image URL is required")
     @URL(protocol = "http", message = "Large image URL must be a valid HTTP URL")
     @Column(name = "image_url_large")
     private String imageUrlLarge;
