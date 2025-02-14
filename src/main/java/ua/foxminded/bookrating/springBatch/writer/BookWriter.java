7d6c7417c2dd026a61e277b0714f325b6922c00a
Kyrylo Artemov <kirillartemov1993@gmail.com>
2025-02-14 10:34:12 +0100

    Refactor BookWriter -Remove BOOK_ISBN variable -Replace 'ItemStreamWriter' with 'ItemWriter' -Remove 'close' method -Add 'ExecutorService' variable for parallel saving -Uses  to persist books in bulk

--- src/main/java/ua/foxminded/bookrating/springBatch/writer/BookWriter.java
+++ src/main/java/ua/foxminded/bookrating/springBatch/writer/BookWriter.java
@@ -1,38 +1,29 @@
 package ua.foxminded.bookrating.springBatch.writer;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ItemStreamException;
-import org.springframework.batch.item.ItemStreamWriter;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 import ua.foxminded.bookrating.persistance.entity.Book;
 import ua.foxminded.bookrating.persistance.repo.BookRepository;
+import ua.foxminded.bookrating.service.BookService;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
-public class BookWriter implements ItemStreamWriter<Book> {
-
-    private static final Set<String> BOOKS_ISBN = new HashSet<>();
+public class BookWriter implements ItemWriter<Book> {
 
     private final BookRepository bookRepository;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     @Override
-    public void write(Chunk<? extends Book> chunk) {
-        log.debug("Processing chunk with {} books.", chunk.getItems().size());
-        Set<Book> books = new HashSet<>(chunk.getItems());
-        books.stream().filter(book -> !BOOKS_ISBN.contains(book.getIsbn()))
-                .forEach(book -> BOOKS_ISBN.add(bookRepository.save(book).getIsbn()));
-        log.debug("{} new books were successfully saved in this chunk.", books.size());
-    }
-
-    @Override
-    public void close() throws ItemStreamException {
-        log.info("Closing writer. Total unique books processed: {}. Clearing state.", BOOKS_ISBN.size());
-        BOOKS_ISBN.clear();
+    public void write(Chunk<? extends Book> chunk) throws ExecutionException, InterruptedException {
+        Future<List<?>> future = executorService.submit(() -> bookRepository.saveAll(chunk.getItems()));
+        future.get();
     }
 }
